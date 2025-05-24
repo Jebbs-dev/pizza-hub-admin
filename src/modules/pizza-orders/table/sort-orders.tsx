01d36124a63769c5776a714f608d1a6b8aac6026
Jebbs-dev <ojebiyifulness@gmail.com>
2025-05-25 00:03:58 +0100

    feat: create component to sort orders by status

---
+++ src/modules/pizza-orders/table/sort-orders.tsx
@@ -0,0 +1,47 @@
+import { PizzaOrders } from "../order-data";
+
+interface SortOrdersStatusProps {
+  data: PizzaOrders;
+}
+
+const SortOrdersStatus = ({ data }: SortOrdersStatusProps) => {
+  return (
+    <span
+      className={`
+      px-3 py-1 flex flex-row shrink-0 space-x-3 items-center rounded-sm w-5/6 
+      ${
+        data.status === "Pending"
+          ? "bg-yellow-100 text-yellow-700 text-opacity-90"
+          : data.status === "Preparing"
+          ? "bg-yellow-200 text-yellow-800 text-opacity-90"
+          : data.status === "Out for Delivery"
+          ? "bg-blue-100 text-blue-700 text-opacity-90"
+          : data.status === "Delivered"
+          ? "bg-green-100 text-green-700 text-opacity-90"
+          : data.status === "Cancelled"
+          ? "bg-red-200 text-red-700 text-opacity-90"
+          : ""
+      }
+    `}
+    >
+      <span
+        className={`w-2 h-2 rounded-full ${
+          data.status === "Pending"
+            ? " bg-yellow-500"
+            : data.status === "Preparing"
+            ? "bg-yellow-700 "
+            : data.status === "Out for Delivery"
+            ? "bg-blue-500"
+            : data.status === "Delivered"
+            ? "bg-green-500"
+            : data.status === "Cancelled"
+            ? "bg-red-500"
+            : ""
+        }`}
+      ></span>
+      <span className="text-sm">{data.status}</span>
+    </span>
+  );
+};
+
+export default SortOrdersStatus;
