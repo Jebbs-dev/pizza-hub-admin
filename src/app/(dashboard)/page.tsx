691fa549f212271cb37ec6f9bda9db9bccab6bd1
Jebbs-dev <ojebiyifulness@gmail.com>
2025-05-24 23:58:59 +0100

    chore: move auth protection to layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,41 +1,14 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { signOut, useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { FaSpinner } from "react-icons/fa6";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    // If there's no session, redirect to the authentication page
-    if (status !== "loading" && !session) {
-      router.push("/auth");
-    }
-  }, [status, session, router]);
-
-  if (status === "loading") {
-    return (
-      <div className="flex justify-center items-center h-screen text-3xl">
-        Loading
-        <FaSpinner className="ml-3 animate-spin" />
-      </div>
-    );
-  }
-
-  if (!session) {
-    // Early return for cases when redirect is in progress
-    return null;
-  }
+  const { data: session } = useSession();
 
   return (
     <div className="container px-6 py-6 h-full">
-      <h1 className="text-xl lg:text-2xl">Hello, {session!.user?.name}</h1>
-      <p className="text-foreground text-sm">Welcome to your Dashboard</p>
-      <Button onClick={() => signOut()}>Sign out</Button>
+      <h1 className="text-xl lg:text-2xl text-orange-400">Hello, {session!.user?.name}</h1>
+      <p className="text-muted-foreground text-sm">Welcome to your Dashboard</p>
     </div>
   );
 }
